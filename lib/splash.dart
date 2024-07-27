ed4ac1a389edbbdc9a5ff3e24a1faf989dc85fa5
crowdfreak <lalubagoes2@gmail.com>
2024-07-27 10:47:02 +0700

    fix fragment view not showing

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:des_uad/core/constant_finals.dart';
+import 'package:des_uad/init_screens/fragment_view.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/home/home.dart';
@@ -15,7 +16,7 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      nextScreen: const HomeScreen(),
+      nextScreen: const FragmentPage(),
       splashTransition: SplashTransition.fadeTransition,
       splashIconSize: 200,
       splash: Center(
